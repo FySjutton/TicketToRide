7c8894b3cbce975f0a761630387b462efefc8f99
Fy17 <rubik.sten@gmail.com>
2025-09-28 20:10:30 +0200

    Added a new ScrollableRow widget

--- src/main/java/avox/test/ticketToRide/guis/game/ViewInfo.java
+++ src/main/java/avox/test/ticketToRide/guis/game/ViewInfo.java
@@ -8,22 +8,21 @@ import avox.test.ticketToRide.guis.GuiAction;
 import avox.test.ticketToRide.guis.GuiTools;
 import avox.test.ticketToRide.guis.InventoryGui;
 import avox.test.ticketToRide.guis.PlayerGuiManager;
+import avox.test.ticketToRide.guis.general.ScrollableRow;
+
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 
 public class ViewInfo extends InventoryGui {
-    private int scroll = 0;
-
     public ViewInfo(Game game, Player user, PlayerGuiManager.PlayerEntry oldState) {
         super(user, 54, Component.text(user.getName() + " Game Info"), oldState);
-
         GamePlayer player = game.gamePlayers.get(user);
 
         gui.setItem(4, GuiTools.format(
@@ -32,86 +31,36 @@ public class ViewInfo extends InventoryGui {
                 game.gamePlayers.values().stream().filter(gamePlayer -> !gamePlayer.equals(player)).map(gamePlayer -> gamePlayer.markerData.colored.append(GuiTools.getGray(" - " + gamePlayer.trains))).toList()
         ));
 
-        int slot = 0;
-        for (MapColor color : player.cards.keySet()) {
-            int cards = player.cards.get(color);
-            ItemStack stack;
-            if (cards == 0) {
-                stack = new ItemStack(Material.BARRIER);
-            } else {
-                stack = new ItemStack(color.material).add(cards - 1);
+        new ScrollableRow<>(actionManager, gui, 9, 9, new ArrayList<>(player.cards.keySet()), null) {
+            @Override
+            public ItemStack getSlotItem(MapColor color) {
+                int cards = player.cards.get(color);
+                ItemStack stack;
+                if (cards == 0) {
+                    stack = new ItemStack(Material.BARRIER);
+                } else {
+                    stack = new ItemStack(color.material).add(cards - 1);
+                }
+                return GuiTools.format(stack, GuiTools.getYellow("Card ").append(color.colored.decoration(TextDecoration.ITALIC, false).decorate(TextDecoration.BOLD)).append(GuiTools.getGray(" (" + cards + ")")));
             }
-            gui.setItem(9 + slot, GuiTools.format(stack, GuiTools.getYellow("Card ").append(color.colored.decoration(TextDecoration.ITALIC, false).decorate(TextDecoration.BOLD)).append(GuiTools.getGray(" (" + cards + ")"))));
-            slot++;
-        }
+        };
 
-        updateDestinationRow(game, gui, player, 27);
-    }
-
-    private void updateDestinationRow(Game game, Inventory gui, GamePlayer player, int slot) {
-        ArrayList<DestinationCard> cards = player.getDestinationCards();
-        int totalCards = cards.size();
-
-        for (int i = 0; i <= 8; i++) {
-            actionManager.removeAction(slot + i);
-            gui.setItem(slot + i, null);
-        }
-
-        boolean hasBack = scroll > 0;
-        boolean hasForward = scroll + (8 - (hasBack ? 1 : 0)) < totalCards;
-
-        int cardSlots = 8 - (hasBack ? 1 : 0) - (hasForward ? 1 : 0);
-        int itemSlot = slot + (hasBack ? 1 : 0);
-
-        if (hasBack) {
-            actionManager.addAction(gui, GuiTools.format(new ItemStack(Material.ARROW), GuiTools.getYellow("Scroll back")), slot,
-                    GuiAction.ofClick(() -> {
-                        if (scroll == 2) scroll = 0;
-                        else scroll--;
-                        updateDestinationRow(game, gui, player, slot);
-                    })
-            );
-        }
-
-        if (hasForward) {
-            actionManager.addAction(gui, GuiTools.format(new ItemStack(Material.ARROW), GuiTools.getYellow("Scroll forward")), slot + 7,
-                    GuiAction.ofClick(() -> {
-                        if (scroll == 0 && totalCards > 7) {
-                            scroll = 2;
-                        } else {
-                            scroll++;
-                        }
-                        updateDestinationRow(game, gui, player, slot);
-                    })
-            );
-        }
-
-        for (int i = 0; i < cardSlots; i++) {
-            actionManager.removeAction(itemSlot);
-            int cardIndex = scroll + i;
-            if (cardIndex < totalCards) {
-                DestinationCard card = cards.get(cardIndex);
-                gui.setItem(itemSlot, GuiTools.format(
-                        new ItemStack(card.finished ? Material.LIME_CONCRETE : Material.RED_CONCRETE),
-                        GuiTools.getYellow(card.pointA.name() + " - " + card.pointB.name() + " (" + card.reward + " points) " + (cardIndex + 1))
-                ));
-            } else {
-                gui.setItem(itemSlot, GuiTools.format(new ItemStack(Material.BARRIER), GuiTools.getYellow("Empty Destination Card Slot")));
+        ScrollableRow<DestinationCard> scrollableRow = new ScrollableRow<>(actionManager, gui, 27, 8, player.getDestinationCards(), GuiTools.getYellow("Empty Destination Card Slot")) {
+            @Override
+            public ItemStack getSlotItem(DestinationCard item) {
+                return GuiTools.format(
+                        new ItemStack(item.finished ? Material.LIME_CONCRETE : Material.RED_CONCRETE),
+                        GuiTools.getYellow(item.pointA.name() + " - " + item.pointB.name() + " (" + item.reward + " points)")
+                );
             }
-            itemSlot++;
-        }
+        };
 
-        actionManager.addAction(gui, GuiTools.format(GuiTools.clearCompass(new ItemStack(Material.COMPASS)), GuiTools.getYellow("Click to view cards on board")), slot + 8,
-                GuiAction.ofClick(() -> {
-                    ArrayList<DestinationCard> visibleCards = new ArrayList<>();
-                    for (int i = 0; i < cardSlots; i++) {
-                        int idx = scroll + i;
-                        if (idx < totalCards) visibleCards.add(cards.get(idx));
-                    }
-                    if (!visibleCards.isEmpty()) {
-                        game.gameHandler.destinationHandler.viewDestinationCards(visibleCards, game, player.player);
-                    }
-                })
+        actionManager.addAction(gui, GuiTools.format(GuiTools.clearCompass(new ItemStack(Material.COMPASS)), GuiTools.getYellow("Click to view cards on board")), 27 + 8,
+            GuiAction.ofClick(() -> {
+                if (!scrollableRow.currentlyShown.isEmpty()) {
+                    game.gameHandler.destinationHandler.viewDestinationCards(scrollableRow.currentlyShown, game, player.player);
+                }
+            })
         );
     }
 }
---
+++ src/main/java/avox/test/ticketToRide/guis/general/ScrollableRow.java
@@ -0,0 +1,95 @@
+package avox.test.ticketToRide.guis.general;
+
+import avox.test.ticketToRide.game.Game;
+import avox.test.ticketToRide.game.player.GamePlayer;
+import avox.test.ticketToRide.guis.ActionManager;
+import avox.test.ticketToRide.guis.GuiAction;
+import avox.test.ticketToRide.guis.GuiTools;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+
+public abstract class ScrollableRow<T> {
+    private final ActionManager actionManager;
+    private final ArrayList<T> objectList;
+    private final Inventory inventory;
+    private final int startSlot;
+    private final int length;
+    private final Component emptySlotName;
+
+    public ArrayList<T> currentlyShown;
+    private int scroll = 0;
+
+    public ScrollableRow(ActionManager actionManager, Inventory gui, int startSlot, int length, ArrayList<T> objectList, Component emptySlotName) {
+        this.actionManager = actionManager;
+        this.objectList = objectList;
+        this.inventory = gui;
+        this.startSlot = startSlot;
+        this.length = length;
+        this.emptySlotName = emptySlotName;
+
+        updateRow();
+    }
+
+    public abstract ItemStack getSlotItem(T item);
+
+    private void updateRow() {
+        int objects = objectList.size();
+
+        for (int i = 0; i <= length; i++) {
+            actionManager.removeAction(startSlot + i);
+            inventory.setItem(startSlot + i, null);
+        }
+
+        boolean hasBack = scroll > 0;
+        boolean hasForward = scroll + (length - (hasBack ? 1 : 0)) < objects;
+
+        int cardSlots = length - (hasBack ? 1 : 0) - (hasForward ? 1 : 0);
+        int itemSlot = startSlot + (hasBack ? 1 : 0);
+
+        if (hasBack) {
+            actionManager.addAction(inventory, GuiTools.format(new ItemStack(Material.ARROW), GuiTools.getYellow("Scroll back")), startSlot,
+                    GuiAction.ofClick(() -> {
+                        if (scroll == 2) scroll = 0;
+                        else scroll--;
+                        updateRow();
+                    })
+            );
+        }
+
+        if (hasForward) {
+            actionManager.addAction(inventory, GuiTools.format(new ItemStack(Material.ARROW), GuiTools.getYellow("Scroll forward")), startSlot + length - 1,
+                GuiAction.ofClick(() -> {
+                    if (scroll == 0 && objects > length - 1) {
+                        scroll = 2;
+                    } else {
+                        scroll++;
+                    }
+                    updateRow();
+                })
+            );
+        }
+
+        for (int i = 0; i < cardSlots; i++) {
+            actionManager.removeAction(itemSlot);
+            int cardIndex = scroll + i;
+            if (cardIndex < objects) {
+                T object = objectList.get(cardIndex);
+                inventory.setItem(itemSlot, getSlotItem(object));
+            } else if (emptySlotName != null) {
+                inventory.setItem(itemSlot, GuiTools.format(new ItemStack(Material.BARRIER), emptySlotName));
+            }
+            itemSlot++;
+        }
+
+        ArrayList<T> visibleObjects = new ArrayList<>();
+        for (int i = 0; i < cardSlots; i++) {
+            int idx = scroll + i;
+            if (idx < objects) visibleObjects.add(objectList.get(idx));
+        }
+        currentlyShown = visibleObjects;
+    }
+}
